81ba020f206db5466297be747158650812c57bac
kurtcapiliaws <capilikurt.aws@gmail.com>
2023-10-03 16:17:02 +0800

    Added MovieReservation.java

--- src/com/aws/moviereservation/MovieReservation.java
+++ src/com/aws/moviereservation/MovieReservation.java
@@ -1,5 +1,49 @@
 package com.aws.moviereservation;
 
-public class MovieReservation {
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 
+public class MovieReservation {
+	private long reservationNo;
+	private LocalDate showingDateTime;
+	private byte cinemaNo;
+	private LocalTime timeStart;
+	private ArrayList<String> reservedSeats;
+	private float totalPrice;
+	
+	public MovieReservation(long reservationNo, LocalDate showingDateTime, byte cinemaNo, LocalTime timeStart,
+			ArrayList<String> reservedSeats, float totalPrice) {
+		this.reservationNo = reservationNo;
+		this.showingDateTime = showingDateTime;
+		this.cinemaNo = cinemaNo;
+		this.timeStart = timeStart;
+		this.reservedSeats = reservedSeats;
+		this.totalPrice = totalPrice;
+	}
+
+	public long getReservationNo() {
+		return reservationNo;
+	}
+
+	public LocalDate getShowingDateTime() {
+		return showingDateTime;
+	}
+
+	public byte getCinemaNo() {
+		return cinemaNo;
+	}
+
+	public LocalTime getTimeStart() {
+		return timeStart;
+	}
+
+	public ArrayList<String> getReservedSeats() {
+		return reservedSeats;
+	}
+
+	public float getTotalPrice() {
+		return totalPrice;
+	}
+	
 }
